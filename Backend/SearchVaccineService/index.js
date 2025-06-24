d630d35df49fa1aab221b9c6d691ca8e2594df64
Leonardo Tagliati <leotagliati56@gmail.com>
2025-06-24 17:00:48 -0300

    feat: melhorando ms

--- Backend/SearchVaccineService/index.js
+++ Backend/SearchVaccineService/index.js
@@ -4,14 +4,65 @@ const app = express();
 app.use(express.json());
 
 const localDB = [
-  { id: 1, name: 'COVID-19', date: '2023-05-10', dose: '1ª Dose' },
-  { id: 2, name: 'Hepatite B', date: '2023-06-20', dose: '2ª Dose' },
+    {
+        username: 'defaultUser',
+        age: 25,
+        vaccines: [
+            {
+                id: 1,
+                name: 'COVID-19',
+                data: '2023-05-10',
+                dose: '1ª Dose',
+            },
+            {
+                id: 2,
+                name: 'Hepatite B',
+                data: '2023-06-20',
+                dose: '2ª Dose',
+            },
+        ]
+    }
+    ,
 ]
+app.post('/vaccines/add', (req, res) => {
+    const { username, vaccine } = req.body;
+    if (!username || !vaccine) {
+        return res.status(400).json({ error: 'Username e vacina são obrigatórios' });
+    }
+    const user = localDB.find(user => user.username === username);
+    if (!user) {
+        return res.status(404).json({ error: 'Usuário não encontrado' });
+    }
+    const newVaccine = {
+        id: user.vaccines.length + 1,
+        ...vaccine
+    };
+    user.vaccines.push(newVaccine);
+    res.status(201).json(newVaccine);
+})
 
 app.get('/vaccines', (req, res) => {
-    res.json(localDB);
+    const { username, vaccineName } = req.query;
+
+    if (!username) {
+        return res.status(400).json({ error: 'Username é obrigatório' });
+    }
+
+    const user = localDB.find(user => user.username === username);
+    if (!user) {
+        return res.status(404).json({ error: 'Usuário não encontrado' });
+    }
+
+    let vaccines = user.vaccines;
+
+    if (vaccineName) {
+        vaccines = vaccines.filter(v => v.name.toLowerCase().includes(vaccineName.toLowerCase()));
+    }
+
+    res.json(vaccines);
 });
 
 
-app.listen(3000, () => {  console.log('Server is running on port 3000');
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
