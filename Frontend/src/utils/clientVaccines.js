2dbcb0ac3063ee1d3a18bdb1b70692369186f384
Leonardo Tagliati <leotagliati56@gmail.com>
2025-06-26 10:12:29 -0300

    feat: interceptor para compor header da req com token

--- Frontend/src/utils/clientVaccines.js
+++ Frontend/src/utils/clientVaccines.js
@@ -6,5 +6,15 @@ const clientVaccines = axios.create({
     'Content-Type': 'application/json',
   },
 });
+clientVaccines.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default clientVaccines;
